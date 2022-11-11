ef2a49a6aff1404a4a478ec1aa735ad471c43016
Maxalbinsson <max.albiknsson@elev.ga.ntig.se>
2022-11-11 13:24:57 +0100

    gjort något

--- src/Ball.java
+++ src/Ball.java
@@ -1,2 +1,17 @@
 public class Ball {
+
+    private int y;
+    private int x;
+    private int vx;
+    private int vy;
+
+    public Ball(int x,y) {
+        this.y = y;
+        this.x = x;
+        this.vx = 1;
+        this.vy = 1;
+    }
+
+
+
 }
